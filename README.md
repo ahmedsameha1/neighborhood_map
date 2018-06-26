70e2b55644a36cfa75939b4ff8a9d68a6a9cca1e
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2018-06-26 06:55:53 +0200

    Add map take 100% of screen

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
+
+//////////////////////  must add google-maps-react on how to setup   ///////////////////////////////////
 ATTRIBUTIONS
 ============
 
 https://medium.com/front-end-hacking/simplified-google-maps-api-in-a-react-app-46981441d2c9
 
 https://www.google.com.eg/
+
+https://www.npmjs.com/package/google-maps-react
+
+https://stackoverflow.com/a/42877693 by: bora89.
+
+https://developers.google.com/maps/documentation/javascript/tutorial
---
+++ src/components/Map.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {GoogleApiWrapper} from "google-maps-react";
+import ReactDOM from "react-dom";
+
+class Map extends React.Component {
+    load_map() {
+        if ( this.props && this.props.google ) {
+            new this.props.google.maps.Map(
+                ReactDOM.findDOMNode(this.refs.map),
+                {
+                    center: { lat: 2, lng: 11},
+                    zoom: 4,
+                });
+        }
+    }
+    componentDidMount(){
+        this.load_map();
+    }
+    render() {
+        return (
+            <div ref="map" className="map">
+            </div>
+        );
+    }
+}
+
+export default GoogleApiWrapper(
+    { apiKey: "AIzaSyDcvTtkitnRqPDCyhTVrmyceRUHgiToN24", }
+)(Map);
